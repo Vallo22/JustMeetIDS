bafb40e36ec1ce816f865178ea9f5f4c6bebc690
Daniele Vallorani <61566266+Vallo22@users.noreply.github.com>
2020-06-22 11:31:50 +0200

    Add files via upload

---
+++ src/main/java/com/project/justmeet/JustMeetApplication.java
@@ -0,0 +1,13 @@
+package com.project.justmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustMeetApplication.class, args);
+	}
+
+}
